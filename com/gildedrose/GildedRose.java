a156e3b8e66082b82d7ebd076adf76244bfbc192
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2018-01-16 09:55:06 +0100

    first commit

---
+++ com/gildedrose/GildedRose.java
@@ -0,0 +1,107 @@
+package com.gildedrose;
+
+class GildedRose {
+    private static final int MIN_QUALITY = 0;
+	private static final int MAX_QUALITY = 50;
+	private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+	private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+	private static final String AGED_BRIE = "Aged Brie";
+	private static final int SELLING_DATE = 0;
+	Item[] items;
+
+    public GildedRose(Item[] items) {
+        this.items = items;
+    }
+
+    public void updateQuality() {
+        for (Item item : items) {
+            if (isAgedBrie(item) || isBackstagePass(item)) {
+                if (isItemQualityBelowMaxQuality(item)) {
+                    increaseQualityByOne(item);
+
+                    if (isBackstagePass(item)) {
+                        if (item.sellIn < 11) {
+                            increaseQuality(item);
+                        }
+
+                        if (item.sellIn < 6) {
+                            increaseQuality(item);
+                        }
+                    }
+                }
+            } else {
+                if (isItemQualityOverMinQuality(item)) {
+                    if (!isSulfuras(item)) {
+                        decreaseQualityByOne(item);
+                    }
+                }
+            }
+
+            if (!isSulfuras(item)) {
+                decreaseSellingDateByOne(item);
+            }
+
+            if (isSellinDatePassed(item)) {
+                if (!isAgedBrie(item)) {
+                    if (!isBackstagePass(item)) {
+                        if (isItemQualityOverMinQuality(item)) {
+                            if (!isSulfuras(item)) {
+                                decreaseQualityByOne(item);
+                            }
+                        }
+                    } else {
+                        setQualityToMinQuality(item);
+                    }
+                } else {
+                    increaseQuality(item);
+                }
+            }
+        }
+    }
+
+	private void increaseQuality(Item item) {
+		if (isItemQualityBelowMaxQuality(item)) {
+		    increaseQualityByOne(item);
+		}
+	}
+
+	private void decreaseSellingDateByOne(Item item) {
+		item.sellIn--;
+	}
+
+	private void decreaseQualityByOne(Item item) {
+		item.quality--;
+	}
+
+	private void setQualityToMinQuality(Item item) {
+		item.quality = MIN_QUALITY;
+	}
+
+	private boolean isSellinDatePassed(Item item) {
+		return item.sellIn < SELLING_DATE;
+	}
+
+	private boolean isItemQualityOverMinQuality(Item item) {
+		return item.quality > MIN_QUALITY;
+	}
+
+	private int increaseQualityByOne(Item item) {
+		return item.quality++;
+	}
+
+	private boolean isItemQualityBelowMaxQuality(Item item) {
+		return item.quality < MAX_QUALITY;
+	}
+
+	private boolean isSulfuras(Item item) {
+		return item.name.equals(SULFURAS);
+	}
+
+	private boolean isBackstagePass(Item item) {
+		return item.name.equals(BACKSTAGE_PASSES);
+	}
+
+	private boolean isAgedBrie(Item item) {
+		return item.name.equals(AGED_BRIE);
+	}
+}
---
+++ com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,114 @@
+package com.gildedrose;
+
+import static org.junit.Assert.*;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+
+public class GildedRoseTest {
+
+	@Test
+	public void whenDayPasses_itemAgedBrie_shouldIncreaseQualityByOne()
+			throws Exception {
+		Item item = new Item(getAgedBrie(), notSellinDatePassed(), 10);
+		GildedRose app = createApp(item);
+
+		app.updateQuality();
+
+		assertThat(item.quality, CoreMatchers.is(11));
+	}
+
+	
+	@Test
+	public void whenDayPasses_normalItem_shouldDecreaseQualityByOne()
+			throws Exception {
+		Item item = new Item(anyName(), notSellinDatePassed(), 10);
+		GildedRose app = createApp(item);
+		app.updateQuality();
+		assertThat(item.quality, CoreMatchers.is(9));
+	}
+
+	@Test
+	public void whenDayPasses_normalItem_shouldDecreaseDaysLeftByOne()
+			throws Exception {
+		Item item = new Item(anyName(), notSellinDatePassed(), 10);
+		GildedRose app = createApp(item);
+		app.updateQuality();
+		assertThat(item.sellIn, CoreMatchers.is(notSellinDatePassed() - 1));
+	}
+	
+	@Test
+	public void whenDayPasses_itemSulfuras__shouldNotChangeQuality() throws Exception {
+		Item item = new Item("Sulfuras, Hand of Ragnaros", notSellinDatePassed(), anyQuality());
+		GildedRose app = createApp(item);
+		app.updateQuality();
+		assertThat(item.quality, CoreMatchers.is(anyQuality()));
+	}
+	
+	@Test
+	public void whenDayPasses_normalItemWithZeroQuality_shouldNotDecreaseQuality()
+			throws Exception {
+		Item item = new Item(anyName(), anySellingDate(), zeroQuality());
+		GildedRose app = createApp(item);
+		app.updateQuality();
+		assertThat(item.quality, CoreMatchers.is(zeroQuality()));
+	}
+	
+	@Test
+	public void whenDayPasses_normalItemWithNegativeSellDate_shouldDecreaseQualityeByTwo()
+			throws Exception {
+		Item item = new Item(anyName(), anyNegativeSellingDate(), anyQuality());
+		GildedRose app = createApp(item);
+		app.updateQuality();
+		assertThat(item.quality, CoreMatchers.is(anyQuality() -2));
+	}
+	
+	@Test
+	public void whenDayPasses_itemAgedBrieWithQualityFifty_shouldNotIncreaseQuality()
+			throws Exception {
+		Item item = new Item(getAgedBrie(), anySellingDate(), getMaxQuality());
+		GildedRose app = createApp(item);
+		app.updateQuality();
+		assertThat(item.quality, CoreMatchers.is(getMaxQuality()));
+	}
+
+	private int anyNegativeSellingDate() {
+		return -1;
+	}
+
+
+	private int anyQuality() {
+		return 10;
+	}
+
+
+	private int getMaxQuality() {
+		return 50;
+	}
+
+	private int zeroQuality() {
+		return 0;
+	}
+
+	private int anySellingDate() {
+		return 0;
+	}
+
+	private GildedRose createApp(Item item) {
+		Item[] items = new Item[] { item };
+		GildedRose app = new GildedRose(items);
+		return app;
+	}
+
+	private String getAgedBrie() {
+		return "Aged Brie";
+	}
+
+	private int notSellinDatePassed() {
+		return 3;
+	}
+
+	private String anyName() {
+		return "foo";
+	}
+}
