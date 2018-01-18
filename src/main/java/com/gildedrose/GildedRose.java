98a96216db5a9a2950c0beb349fcfb8bb807e17a
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-01-18 21:19:19 +0100

    apply consistent formatting
    bring in testing libraries

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -59,49 +59,49 @@ class GildedRose {
         }
     }
 
-	private void increaseQuality(Item item) {
-		if (isItemQualityBelowMaxQuality(item)) {
-		    increaseQualityByOne(item);
-		}
-	}
+    private void increaseQuality(Item item) {
+        if (isItemQualityBelowMaxQuality(item)) {
+            increaseQualityByOne(item);
+        }
+    }
 
-	private void decreaseSellingDateByOne(Item item) {
-		item.sellIn--;
-	}
+    private void decreaseSellingDateByOne(Item item) {
+        item.sellIn--;
+    }
 
-	private void decreaseQualityByOne(Item item) {
-		item.quality--;
-	}
+    private void decreaseQualityByOne(Item item) {
+        item.quality--;
+    }
 
-	private void setQualityToMinQuality(Item item) {
-		item.quality = MIN_QUALITY;
-	}
+    private void setQualityToMinQuality(Item item) {
+        item.quality = MIN_QUALITY;
+    }
 
-	private boolean isSellinDatePassed(Item item) {
-		return item.sellIn < SELLING_DATE;
-	}
+    private boolean isSellinDatePassed(Item item) {
+        return item.sellIn < SELLING_DATE;
+    }
 
-	private boolean isItemQualityOverMinQuality(Item item) {
-		return item.quality > MIN_QUALITY;
-	}
+    private boolean isItemQualityOverMinQuality(Item item) {
+        return item.quality > MIN_QUALITY;
+    }
 
-	private int increaseQualityByOne(Item item) {
-		return item.quality++;
-	}
+    private int increaseQualityByOne(Item item) {
+        return item.quality++;
+    }
 
-	private boolean isItemQualityBelowMaxQuality(Item item) {
-		return item.quality < MAX_QUALITY;
-	}
+    private boolean isItemQualityBelowMaxQuality(Item item) {
+        return item.quality < MAX_QUALITY;
+    }
 
-	private boolean isSulfuras(Item item) {
-		return item.name.equals(SULFURAS);
-	}
+    private boolean isSulfuras(Item item) {
+        return item.name.equals(SULFURAS);
+    }
 
-	private boolean isBackstagePass(Item item) {
-		return item.name.equals(BACKSTAGE_PASSES);
-	}
+    private boolean isBackstagePass(Item item) {
+        return item.name.equals(BACKSTAGE_PASSES);
+    }
 
-	private boolean isAgedBrie(Item item) {
-		return item.name.equals(AGED_BRIE);
-	}
+    private boolean isAgedBrie(Item item) {
+        return item.name.equals(AGED_BRIE);
+    }
 }
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,100 +7,98 @@ import org.junit.Test;
 
 public class GildedRoseTest {
 
-	@Test
-	public void whenDayPasses_itemAgedBrie_shouldIncreaseQualityByOne() {
-		Item item = new Item(getAgedBrie(), notSellInDatePassed(), 10);
-		GildedRose app = createApp(item);
+    @Test
+    public void whenDayPasses_itemAgedBrie_shouldIncreaseQualityByOne() {
+        Item item = new Item(getAgedBrie(), notSellInDatePassed(), 10);
+        GildedRose app = createApp(item);
 
-		app.updateQuality();
+        app.updateQuality();
 
-		assertThat(item.quality, CoreMatchers.is(11));
-	}
-	
-	@Test
-	public void whenDayPasses_normalItem_shouldDecreaseQualityByOne() {
-		Item item = new Item(anyName(), notSellInDatePassed(), 10);
-		GildedRose app = createApp(item);
-		app.updateQuality();
-		assertThat(item.quality, CoreMatchers.is(9));
-	}
+        assertThat(item.quality, CoreMatchers.is(11));
+    }
 
-	@Test
-	public void whenDayPasses_normalItem_shouldDecreaseDaysLeftByOne() {
-		Item item = new Item(anyName(), notSellInDatePassed(), 10);
-		GildedRose app = createApp(item);
-		app.updateQuality();
-		assertThat(item.sellIn, CoreMatchers.is(notSellInDatePassed() - 1));
-	}
-	
-	@Test
-	public void whenDayPasses_itemSulfuras__shouldNotChangeQuality() {
-		Item item = new Item("Sulfuras, Hand of Ragnaros", notSellInDatePassed(), anyQuality());
-		GildedRose app = createApp(item);
-		app.updateQuality();
-		assertThat(item.quality, CoreMatchers.is(anyQuality()));
-	}
-	
-	@Test
-	public void whenDayPasses_normalItemWithZeroQuality_shouldNotDecreaseQuality() {
-		Item item = new Item(anyName(), anySellingDate(), zeroQuality());
-		GildedRose app = createApp(item);
-		app.updateQuality();
-		assertThat(item.quality, CoreMatchers.is(zeroQuality()));
-	}
-	
-	@Test
-	public void whenDayPasses_normalItemWithNegativeSellDate_shouldDecreaseQualityByTwo() {
-		Item item = new Item(anyName(), anyNegativeSellingDate(), anyQuality());
-		GildedRose app = createApp(item);
-		app.updateQuality();
-		assertThat(item.quality, CoreMatchers.is(anyQuality() -2));
-	}
-	
-	@Test
-	public void whenDayPasses_itemAgedBrieWithQualityFifty_shouldNotIncreaseQuality() {
-		Item item = new Item(getAgedBrie(), anySellingDate(), getMaxQuality());
-		GildedRose app = createApp(item);
-		app.updateQuality();
-		assertThat(item.quality, CoreMatchers.is(getMaxQuality()));
-	}
+    @Test
+    public void whenDayPasses_normalItem_shouldDecreaseQualityByOne() {
+        Item item = new Item(anyName(), notSellInDatePassed(), 10);
+        GildedRose app = createApp(item);
+        app.updateQuality();
+        assertThat(item.quality, CoreMatchers.is(9));
+    }
 
-	private int anyNegativeSellingDate() {
-		return -1;
-	}
+    @Test
+    public void whenDayPasses_normalItem_shouldDecreaseDaysLeftByOne() {
+        Item item = new Item(anyName(), notSellInDatePassed(), 10);
+        GildedRose app = createApp(item);
+        app.updateQuality();
+        assertThat(item.sellIn, CoreMatchers.is(notSellInDatePassed() - 1));
+    }
 
+    @Test
+    public void whenDayPasses_itemSulfuras__shouldNotChangeQuality() {
+        Item item = new Item("Sulfuras, Hand of Ragnaros", notSellInDatePassed(), anyQuality());
+        GildedRose app = createApp(item);
+        app.updateQuality();
+        assertThat(item.quality, CoreMatchers.is(anyQuality()));
+    }
 
-	private int anyQuality() {
-		return 10;
-	}
+    @Test
+    public void whenDayPasses_normalItemWithZeroQuality_shouldNotDecreaseQuality() {
+        Item item = new Item(anyName(), anySellingDate(), zeroQuality());
+        GildedRose app = createApp(item);
+        app.updateQuality();
+        assertThat(item.quality, CoreMatchers.is(zeroQuality()));
+    }
 
+    @Test
+    public void whenDayPasses_normalItemWithNegativeSellDate_shouldDecreaseQualityByTwo() {
+        Item item = new Item(anyName(), anyNegativeSellingDate(), anyQuality());
+        GildedRose app = createApp(item);
+        app.updateQuality();
+        assertThat(item.quality, CoreMatchers.is(anyQuality() - 2));
+    }
 
-	private int getMaxQuality() {
-		return 50;
-	}
+    @Test
+    public void whenDayPasses_itemAgedBrieWithQualityFifty_shouldNotIncreaseQuality() {
+        Item item = new Item(getAgedBrie(), anySellingDate(), getMaxQuality());
+        GildedRose app = createApp(item);
+        app.updateQuality();
+        assertThat(item.quality, CoreMatchers.is(getMaxQuality()));
+    }
 
-	private int zeroQuality() {
-		return 0;
-	}
+    private int anyNegativeSellingDate() {
+        return -1;
+    }
 
-	private int anySellingDate() {
-		return 0;
-	}
+    private int anyQuality() {
+        return 10;
+    }
 
-	private GildedRose createApp(Item item) {
-		Item[] items = new Item[] { item };
-		return new GildedRose(items);
-	}
+    private int getMaxQuality() {
+        return 50;
+    }
 
-	private String getAgedBrie() {
-		return "Aged Brie";
-	}
+    private int zeroQuality() {
+        return 0;
+    }
 
-	private int notSellInDatePassed() {
-		return 3;
-	}
+    private int anySellingDate() {
+        return 0;
+    }
 
-	private String anyName() {
-		return "foo";
-	}
+    private GildedRose createApp(Item item) {
+        Item[] items = new Item[]{item};
+        return new GildedRose(items);
+    }
+
+    private String getAgedBrie() {
+        return "Aged Brie";
+    }
+
+    private int notSellInDatePassed() {
+        return 3;
+    }
+
+    private String anyName() {
+        return "foo";
+    }
 }
